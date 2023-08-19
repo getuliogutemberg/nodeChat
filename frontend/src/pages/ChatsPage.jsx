40258942e3bf7054ecd2f314ddba82cb551f8ee6
Getulio Gutemberg <getulio.dev@gmail.com>
2023-08-19 20:39:27 -0300

    adiciona outra lib react chat

--- frontend/src/pages/ChatsPage.jsx
+++ frontend/src/pages/ChatsPage.jsx
@@ -1,10 +1,17 @@
 import {MultiChatSocket,MultiChatWindow,useMultiChatLogic} from 'react-chat-engine-advanced';
+import { PrettyChatWindow } from 'react-chat-engine-pretty';
 const ChatsPage = (props) => {
     const chatProps = useMultiChatLogic('ced49730-4632-4088-9ff5-f51317295a3f',props.user.username,props.user.secret);
     return (
     <div style={{height:'100vh'}}>   
-        <MultiChatWindow {...chatProps} /> 
-        <MultiChatSocket {...chatProps} style={{height:'100%'}}/>
+        {/* <MultiChatWindow {...chatProps} /> 
+        <MultiChatSocket {...chatProps} style={{height:'100%'}}/> */}
+        <PrettyChatWindow 
+            projectId='ced49730-4632-4088-9ff5-f51317295a3f'
+            style={{height:'100%'}}
+            secret={props.user.secret}
+            username={props.user.username}
+        />
     </div>
     )
 }
