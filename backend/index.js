3eb47998ce35a0979328302e4e74412fe81c5bcb
Getulio Gutemberg <getulio.dev@gmail.com>
2023-08-19 19:47:50 -0300

    integra o backend ao chatengine.io

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const { default: axios } = require('axios');
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
+
 app.post('/authenticate', async (req, res) => {
     const { username } = req.body;
-    return res.json({ username: username ,secret: "sha256..."});
+
+    try {
+        const response = await axios.put(
+            `https://api.chatengine.io/users/`, 
+        { username: username, secret: username, first_name: username },
+        { headers: { "private-key": "83f37321-6d8a-44a5-a7b5-9c46e3dee25a" }}
+        );
+        return res.status(response.status).json(response.data);
+    } catch (error) {
+        return res.status(error.response.status).json(error.response.data);
+    }
+     
+    
 })
 
 app.listen(3001, () => {
